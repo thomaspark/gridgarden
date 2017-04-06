927bb77e663e8b5dfbb6ad53fc06c476babf4927
Dan Weatherbee <dan@jedi-clan.us>
2017-04-06 10:46:59 -0700

    feat: progress bar to direct your eyes to animation.

--- js/grid.js
+++ js/grid.js
@@ -1,19 +1,35 @@
-
-
 /*
 Query <div id="overlay"> element and assign variable.
 use for loop to append <span class="plot"></span> elements
 for code efficiency.
 */
-
+var $;
 var overlayElem = $('#overlay');
 var soilElem = $('#soil');
-var createGrid = function () {
+var createGrid = function() {
+    "use strict";
 
-	for (var i = 0;i < 35;i++) {
-	overlayElem.append('<span class="plot"></span>');
-	soilElem.append('<span class="plot"></span>');
-	};
+    for (var i = 0; i < 35; i++) {
+        overlayElem.append('<span class="plot"></span>');
+        soilElem.append('<span class="plot"></span>');
+    }
 
 };
-createGrid();
+
+
+var move = function() {
+    "use strict";
+
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 10);
+
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.style.width = width + '%';
+        }
+    }
+};
